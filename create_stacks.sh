30ac488f57755360e2c5f24f566d49802043cb87
snisioi <sergiu.nisioi@oracle.com>
2017-11-21 11:17:02 +0200

    minor updates

--- create_stacks.sh
+++ create_stacks.sh
@@ -10,9 +10,9 @@ echo "Installing netcat on "$3" and deploying the rollingupdate tool and the doc
 
 scp public-yum-ol6.repo opc@$3:/tmp/public-yum-ol6.repo
 scp yum.conf opc@$3:/tmp/yum.conf
-ssh opc@$3 sudo mv /tmp/public-yum-ol6.repo /etc/yum.repos.d/public-yum-ol6.repo
-ssh opc@$3 sudo mv /tmp/yum.conf /etc/yum.conf
-ssh opc@$3 sudo yum -y install nc
+ssh occs@$3 sudo mv /tmp/public-yum-ol6.repo /etc/yum.repos.d/public-yum-ol6.repo
+ssh occs@$3 sudo mv /tmp/yum.conf /etc/yum.conf
+ssh occs@$3 sudo yum -y install nc
 
 sed -i.bak 's/admin_address/'$3'/g' create_rollingupdate.json
 
